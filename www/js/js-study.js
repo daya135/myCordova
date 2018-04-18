a1f47901dc7b4852a71e8727072cd21b320c3415
daya135 <Jzzperson@163.com>
2018-04-18 18:14:09 +0800

    学习vue.js

--- www/js/js-study.js
+++ www/js/js-study.js
@@ -17,3 +17,45 @@ function myBrowser(){
         alert("IE");
     }; //判断是否IE浏览器
 }
+
+/*
+ * 练习数组和对象的创建方式
+ */
+function arrayAndObjext(){
+	
+	var a = new Array({key:"jzz", value:"100"}, {key:"jzl", value:"1000"});
+	for (var i = 0; i < a.length; i++) {
+		console.log(a[i].key + " " + a[i].value);
+	}
+	var b = new Array("a", "b", "c");
+	for (var i = 0; i < b.length; i++) {
+		console.log(b[i]);
+	}
+	
+	var c = (1, 2, 3);	//这种方式是错误的数组定义方法
+	for (var i = 0; i < c.length; i++) {
+		console.log(c[i]);
+	}
+
+	var d = new Array();
+	d[0] = 1;
+	d[1] = 2;
+	for (var i = 0; i < d.length; i++) {
+		console.log(d[i]);
+	}
+
+	//创建对象方法1
+	var person=new Object();
+	person.firstname="Bill";
+	person.lastname="Gates";
+	person.age=56;
+	person.toString=function(){
+		return this.firstname + this.lastname + this.age;
+	}
+	console.log(person);
+	console.log(person.toString());
+	//创建对象方法2
+	var person={firstname:"John",lastname:"Doe",age:50,eyecolor:"blue"};
+	console.log(person);
+	
+}
